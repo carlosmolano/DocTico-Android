c6e3af110d7c9e7e17763b44c9ea70694feb8984
jorge13mtb <jorge13mtb@gmail.com>
2014-04-07 03:20:43 -0600

    Interfaz agregar cita

--- src/com/example/doctico/AgregarCitaActivity.java
+++ src/com/example/doctico/AgregarCitaActivity.java
@@ -1,14 +1,27 @@
 package com.example.doctico;
 
+import java.util.Calendar;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AgregarCitaActivity extends Activity {
+	
+	EditText miTiempo;
+	EditText miFecha;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_agregar_cita);
 
+		miTiempo = (EditText) findViewById(R.id.texto_hora);
+		miFecha = (EditText) findViewById(R.id.texto_fecha);
+		
+		Calendar calendar = Calendar.getInstance();
+		miTiempo.setText(calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE));
+		miFecha.setText(calendar.get(Calendar.DAY_OF_MONTH) + "/" + calendar.get(Calendar.MONTH) + "/" +  calendar.get(Calendar.YEAR));
+		
 	}
 }
